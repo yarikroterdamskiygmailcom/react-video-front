bfd0582209042c2811409c61a9861e2a99e25d51
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2018-08-30 11:47:19 +0200

    move entrypoints to src

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+import {Provider} from 'mobx-react';
+import {Router} from 'react-router';
+import {AddVlogStore, VlogEditorStore, SessionStore, ProfileStore, VlogsStore, AssetsStore, VlogConfigStore, VlogRenderStore} from './stores';
+import App from './containers/App';
+
+export const history = createBrowserHistory();
+
+const addVlogStore = new AddVlogStore();
+const assetsStore = new AssetsStore();
+const profileStore = new ProfileStore();
+const sessionStore = new SessionStore();
+const vlogConfigStore = new VlogConfigStore();
+const vlogEditorStore = new VlogEditorStore();
+const vlogRenderStore = new VlogRenderStore();
+const vlogsStore = new VlogsStore();
+
+sessionStore.initialize();
+
+const stores = {
+  addVlog: addVlogStore,
+  assets: assetsStore,
+  profile: profileStore,
+  session: sessionStore,
+  vlogConfig: vlogConfigStore,
+  vlogEditor: vlogEditorStore,
+  vlogRender: vlogRenderStore,
+  vlogs: vlogsStore
+};
+
+const renderApp = () =>
+  ReactDOM.render(
+    <Provider {...stores}>
+      <Router history={history}>
+        <App />
+      </Router>
+    </Provider>,
+    document.getElementById('app')
+  );
+
+renderApp();
+
+module.hot.accept(renderApp);
