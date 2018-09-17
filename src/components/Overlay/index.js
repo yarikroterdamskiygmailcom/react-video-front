5d4e081cbc9395afa8a8a7171c0957d0a7226d63
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2018-09-17 15:25:58 +0200

    fix overlay close

--- src/components/Overlay/index.js
+++ src/components/Overlay/index.js
@@ -3,9 +3,16 @@ import styles from './styles.scss';
 
 export default class Overlay extends Component {
 
+  constructor(props) {
+    super(props);
+    this.ref = React.createRef();
+  }
+
+  onClose = e => e.target.isEqualNode(this.ref.current) && this.props.onClose()
+
   render() {
     return (
-      this.props.active && <div className={styles.container}>
+      this.props.active && <div ref={this.ref} className={styles.container} onClick={this.onClose}>
         {this.props.content}
       </div>
     );
