a0d2bcd553acd13726bfcd30f4a2fd21c3c10bed
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2018-09-21 14:03:18 +0200

    revert carousel from translatex to overflow

--- src/atoms/Carousel/index.js
+++ src/atoms/Carousel/index.js
@@ -1,42 +1,22 @@
 import React, {Component} from 'react';
-import Swipeable from 'react-swipeable';
-import {clamp} from 'lodash-es';
 import styles from './styles.scss';
 
 export default class Carousel extends Component {
 
   constructor(props) {
     super(props);
-    this.itemsRef = React.createRef();
-    this.width =
-      this.state = {
-        offset: 0,
-      };
+    this.carouselRef = React.createRef();
   }
 
-  componentDidUpdate() {
-    this.width = this.itemsRef.current.scrollWidth;
-  }
-
-  swipe = (e, deltaX) => this.setState({
-    deltaX
-  });
-
-  postSwipe = (e, deltaX) => this.setState({
-    offset: this.state.offset - deltaX <= 0 ? this.state.offset - deltaX : 0,
-    deltaX: 0
-  });
+  onScroll = e => this.carouselRef.current.scrollLeft += (e.deltaY * 3)
 
   render() {
-    const {offset, deltaX} = this.state;
     return (
       <div className={styles.container}>
         <div className={styles.header}>{this.props.title}</div>
-        <Swipeable onSwiping={this.swipe} onSwiped={this.postSwipe} trackMouse>
-          <div className={styles.items} ref={this.itemsRef} style={{transform: `translateX(${offset - deltaX}px)`}}>
-            {this.props.items.map(this.props.renderFunction)}
-          </div>
-        </Swipeable>
+        <div ref={this.carouselRef} className={styles.items} onWheel={this.onScroll}>
+          {this.props.items.map(this.props.renderFunction)}
+        </div>
       </div>
     );
   }
