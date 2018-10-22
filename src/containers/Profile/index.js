5261f5d1cc6b86e6503dc7a5ad199b28eeea3764
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2018-10-22 14:17:06 +0200

    implement avatar and logo on profile

--- src/containers/Profile/index.js
+++ src/containers/Profile/index.js
@@ -7,6 +7,7 @@ import classNames from 'classnames';
 import {Overlay, Preview, Modal} from '../../components';
 import {withRouter} from 'react-router';
 import FontAwesome from 'react-fontawesome';
+import placeholder from './profile-placeholder.png';
 
 @withRouter
 @inject('profile')
@@ -35,9 +36,14 @@ export default class Profile extends Component {
 
   renderPersona = () => {
     const {first_name, last_name, team} = this.props.profile.user;
+    const {avatar, logo} = this.props.profile;
     return (
       <div className={styles.persona}>
-        <div className={styles.avatar} />
+        <div className={styles.avatar} style={{backgroundImage: `url(${avatar || placeholder})`}}>
+          <div className={styles.logoWrapper}>
+            <img className={styles.logo} src={logo}/>
+          </div>
+        </div>
         <div className={styles.fullName}>{`${first_name} ${last_name}`}</div>
         <div className={styles.companyName}>{team}</div>
       </div>
