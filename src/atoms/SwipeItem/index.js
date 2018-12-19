2a0f90a200299a50ae795632e68b6e16419dff0f
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2018-12-19 22:00:20 +0100

    oops indentation

--- src/atoms/SwipeItem/index.js
+++ src/atoms/SwipeItem/index.js
@@ -10,36 +10,36 @@ export default class SwipeItem extends Component {
     this.props.afterAction && this.props.afterAction();
   }
 
-    getActionsWidth = side => side === this.props.reveal
-      ? `${this.props.actions[side].length * 80}px`
-      : '0px'
+  getActionsWidth = side => side === this.props.reveal
+    ? `${this.props.actions[side].length * 80}px`
+    : '0px'
 
-      renderAction = (action, i) => (
-        <div key={i} className={styles.action} onClick={this.callAction(action.func)}>
-          {action.label}
-        </div>
-      )
+  renderAction = (action, i) => (
+    <div key={i} className={styles.action} onClick={this.callAction(action.func)}>
+      {action.label}
+    </div>
+  )
 
-      render() {
-        const {onSwipe, actions: {left, right}, children, className} = this.props;
-        return (
-          <div className={styles.container}>
-            {left && <div className={styles.actions} style={{width: this.getActionsWidth('left')}}>
-              {left.map(this.renderAction)}
-            </div>}
-            <Swipeable
-              trackMouse
-              className={className}
-              onSwipedLeft={onSwipe('right')}
-              onSwipedRight={onSwipe('left')}
-            >
-              {children}
-            </Swipeable>
-            {right && <div className={styles.actions} style={{width: this.getActionsWidth('right')}}>
-              {right.map(this.renderAction)}
-            </div>}
-          </div>
+  render() {
+    const {onSwipe, actions: {left, right}, children, className} = this.props;
+    return (
+      <div className={styles.container}>
+        {left && <div className={styles.actions} style={{width: this.getActionsWidth('left')}}>
+          {left.map(this.renderAction)}
+        </div>}
+        <Swipeable
+          trackMouse
+          className={className}
+          onSwipedLeft={onSwipe('right')}
+          onSwipedRight={onSwipe('left')}
+        >
+          {children}
+        </Swipeable>
+        {right && <div className={styles.actions} style={{width: this.getActionsWidth('right')}}>
+          {right.map(this.renderAction)}
+        </div>}
+      </div>
 
-        );
-      }
+    );
+  }
 }
