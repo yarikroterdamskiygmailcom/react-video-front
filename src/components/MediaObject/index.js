51e03a4e9ecb134783dc33eba365c27ab83a13f3
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2018-12-21 15:32:26 +0100

    improved mediaobject styles

--- src/components/MediaObject/index.js
+++ src/components/MediaObject/index.js
@@ -5,7 +5,15 @@ import {SortableHandle} from 'react-sortable-hoc';
 import classNames from 'classnames';
 import styles from './styles.scss';
 import {observer, inject} from 'mobx-react';
-import {Preview, SelectAsset, EditTitle} from '../';
+import {Preview, SelectAsset, EditTitle, EditFade} from '../';
+
+const formatTime = number => {
+  const minutes = Math.floor(number / 60);
+  const seconds = Math.floor(number % 60);
+  return `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+};
+
+const isFade = mediatype => ['fadein', 'fadeout', 'fadeoutin', 'crossfade'].includes(mediatype);
 
 const DragHandle = SortableHandle(() =>
   <div className={styles.handle}>
@@ -16,7 +24,11 @@ const DragHandle = SortableHandle(() =>
 const getOverlay = mediatype => ({
   video: Preview,
   asset: SelectAsset,
-  title: EditTitle
+  title: EditTitle,
+  fadein: EditFade,
+  fadeout: EditFade,
+  fadeoutin: EditFade,
+  crossfade: EditFade
 }[mediatype]);
 
 @inject('overlay')
@@ -26,7 +38,8 @@ export default class MediaObject extends Component {
   getOverlayProps = mediaObj => ({
     onSave: this.props.onChange,
     ...{src: mediaObj.mediatype === 'video' ? mediaObj.src : undefined},
-    ...{title: mediaObj.mediatype === 'title' ? mediaObj : undefined}
+    ...{title: mediaObj.mediatype === 'title' ? mediaObj : undefined},
+    ...{fade: isFade(mediaObj.mediatype) ? mediaObj : undefined}
   })
 
   renderThumb = mediaObj => {
@@ -34,30 +47,53 @@ export default class MediaObject extends Component {
       ? {}
       : {onClick: this.props.overlay.openOverlay(getOverlay(mediaObj.mediatype))(this.getOverlayProps(mediaObj))};
 
-    if (mediaObj.mediatype === 'video') {
+    if (['video', 'asset'].includes(mediaObj.mediatype)) {
       return <img className={styles.thumb} src={mediaObj.thumb} {...onClick}/>;
+    }
+
+    if(isFade(mediaObj.mediatype)) {
+      return <Icon className={styles.icon} name="fade" {...onClick}/>;
     }
 
     return <Icon className={styles.icon} name={mediaObj.mediatype} {...onClick}/>;
   }
+
+  renderVideoDesc = video => (
+    <div className={styles.videoDesc}>
+      <div className={styles.oldDuration}>{video.duration}</div>
+      <div className={styles.newDuration}>
+        <Icon className={styles.icon} name="trim"/>
+        <div>{formatTime(video.outpoint - video.inpoint)}</div>
+      </div>
+    </div>
+  )
+
   renderMeta = mediaObj => {
-    const {mediatype, videoname, title, text, duration} = mediaObj;
+    const {mediatype, videoname, title, text, inpoint, outpoint, duration, seconds} = mediaObj;
     const chosenTitle = {
       video: videoname,
       asset: title,
-      title: text
+      title: text,
+      fadein: 'Fade-In',
+      fadeout: 'Fade-Out',
+      fadeoutin: 'Fade-Out / Fade-In',
+      crossfade: 'Crossfade'
     }[mediatype];
 
     const chosenDesc = {
-      video: duration,
+      video: seconds > outpoint - inpoint ? this.renderVideoDesc(mediaObj) : duration,
       asset: '',
-      title: duration
+      title: duration,
+      fadein: formatTime(duration),
+      fadeout: formatTime(duration),
+      fadeoutin: formatTime(duration),
+      crossfade: formatTime(duration)
     }[mediatype];
 
     return (
       <div className={styles.meta}>
         <div className={styles.title}>{`(${mediatype}) ${chosenTitle}`}</div>
-        <div className={styles.desc}>{`${chosenDesc}`}</div>
+        <div className={styles.desc}>{chosenDesc}</div>
       </div>
     );
   }
