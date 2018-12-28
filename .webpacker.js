a3697855b5b257d05e0f99e8ad08c1781b94859a
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2018-12-28 11:15:37 +0100

    fix webpacker publicPath

--- .webpacker.js
+++ .webpacker.js
@@ -3,7 +3,10 @@ const {setLoader, setPlugin} = require('webpacker/utils');
 const scssVariables = path.resolve(__dirname, './src/scss/_variables.scss');
 
 module.exports = {
-  output: fn => fn({path: path.join(__dirname, 'build')}),
+  output: fn => fn({
+    path: path.join(__dirname, 'build'),
+    publicPath: '/'
+  }),
   preset: {
     loaders: [
       setLoader('react'),
