d76573280fef48a26349917da9200c9469912845
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2019-01-23 22:11:35 +0100

    Fix endpoint trailing slashes

--- config/development.js
+++ config/development.js
@@ -2,7 +2,7 @@ const defaults = require('./defaults');
 
 module.exports = {
   ...defaults,
-  videodb: 'https://videodb.vlogahead.cloud/api/v1/',
-  userdb: 'https://userdb.vlogahead.cloud/api/v1/'
+  videodb: 'https://videodb.vlogahead.cloud/api/v1',
+  userdb: 'https://userdb.vlogahead.cloud/api/v1'
 };
 
--- config/production.js
+++ config/production.js
@@ -2,7 +2,7 @@ const defaults = require('./defaults');
 
 module.exports = {
   ...defaults,
-  videodb: 'https://vdb.vlogahead.cloud/api/v1/',
-  userdb: 'https://userdb.vlogahead.cloud/api/v1/'
+  videodb: 'https://vdb.vlogahead.cloud/api/v1',
+  userdb: 'https://userdb.vlogahead.cloud/api/v1'
 };
 
